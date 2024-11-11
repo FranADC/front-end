b6cb42091f2bb9717653a2d8ae790fb32ff4e747
FranADC <franacunadacosta@gmail.com>
2024-11-11 20:40:00 +0100

    Esquema basico de la página, lista de conjuros básica y aceso a conjuros determinados por id

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 //router
-import { BrowserRouter, Routes, Route, json } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  json,
+  useParams,
+} from "react-router-dom";
 //Componentes
 import CompoHeader from "./components/Header";
 import CompoFooter from "./components/Footer";
@@ -13,6 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/magia" element={<Magia />} />
+        <Route path="/magia/conjuros/:ID" element={<Conjuro />} />
         <Route path="/pagina2" element={<Pagina2 />} />
       </Routes>
       <CompoFooter />
@@ -31,7 +38,7 @@ function Magia() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const peticion = await fetch("http://localhost:3000/db");
+        const peticion = await fetch("http://localhost:3000/conjuros");
         if (!peticion.ok) {
           throw new Error("Network response was not ok");
         }
@@ -42,23 +49,73 @@ function Magia() {
       }
     }
 
-    fetchData();
+    fetchData(); //ejecutar la funcion de arriba
   }, []); //[] para que se carge cuando se inicia la página
 
   return (
     <>
+      {console.log(dbMagia)}
       {dbMagia.length === 0 ? (
         <p>No hay usuarios disponibles.</p>
       ) : (
-        dbMagia.map((hechizo) => (
-          <div key={hechizo.ID}>
-            <p>{hechizo.nombre}</p>
-            {/* Muestra aquí otros datos del usuario */}
-          </div>
-        ))
+        <table>
+          <tr>
+            <th>Nombre</th>
+            <th>Nivel</th>
+            <th>Escuela</th>
+            <th>Tiempo de lanzamiento</th>
+            <th>Rango/Area</th>
+            <th>Somático</th>
+            <th>Verbal</th>
+            <th>Material</th>
+          </tr>
+          {dbMagia.map((conjuro) => (
+            <tr>
+              <th>
+                <a href={"/magia/conjuros/" + conjuro.ID_conjuro}>
+                  <img src={conjuro.Imagen_conjuro} alt="" />
+                </a>
+              </th>
+              <th>{conjuro.Nombre_conjuro}</th>
+              <th>{conjuro.Nivel_conjuro}</th>
+              <th>{conjuro.Nombre_escuela}</th>
+              <th>{conjuro.Nombre_lanzamiento}</th>
+              <th>{conjuro.RangoArea}</th>
+              <th>{conjuro.Somatico}</th>
+              <th>{conjuro.Verbal}</th>
+              <th>{conjuro.Material}</th>
+            </tr>
+
+            /* Muestra aquí otros datos del usuario <div dangerouslySetInnerHTML={{ __html: conjuro.DescLarga }} />*/
+          ))}
+        </table>
       )}
     </>
   );
+}
+
+function Conjuro() {
+  const { ID } = useParams();
+
+  const [dbConjuro, setDbConjuro] = useState([]);
+
+  //cuando una variable sufre un cambio (la variable en []), se ejecuta el codigo del interior.`http://localhost:3000/conjuro/${ID}`
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const peticion = await fetch("http://localhost:3000/conjuro/" + ID);
+        if (!peticion.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await peticion.json();
+        setDbMagia(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData(); //ejecutar la funcion de arriba
+  }, []); //[] para que se carge cuando se inicia la página
 }
 
 function Pagina2() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,52 @@
+import "./Header.css";
+import Logo from "../imagenes/marca/logo.png";
+
 export default function () {
   return (
     <header>
-      <p>Cabecera</p>
+      <div className="cabeceraDiv">
+        <div className="cabPrincipal">
+          <img className="cabPrincipalLogo" src={Logo} alt="" />
+          <div>
+            <button>sesión</button>
+          </div>
+        </div>
+        <div className="cabNavegacion">
+          <ul className="cabNavegacionMenu">
+            <li>
+              Como jugar
+              <ul>
+                <li>
+                  <a href="">Crear un personaje</a>
+                </li>
+                <li>
+                  <a href="">Conceptos básicos</a>
+                </li>
+                <li>
+                  <a href="">Conceptos avanzados</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              Reglas
+              <ul>
+                <li>
+                  <a href="">Classes</a>
+                </li>
+                <li>
+                  <a href="">Razas</a>
+                </li>
+                <li>
+                  <a href="">Transfondos</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/magia">magia</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 }
